6b5c9c1c0ae8c4ae1e985d06d793197bd5eabc99
dengliang <dengliang@followme.com>
2018-12-01 15:44:15 +0800

    [misc]更新使用示例

--- main.c
+++ main.c
@@ -9,24 +9,25 @@
 #include <stdio.h>
 #include "regex.h"
 
+/*
+ * 正则表达式程序使用示例
+ * */
 int main(int argc, const char * argv[]) {
-    /* 正则表达式程序使用示例 */
-    
-    const char *regex1 = "5*";
-    Vertex *regexObj1 = CreateRegexObj(regex1);
-    bool result1 = RegexMatch("55555", regexObj1);
+    /* 手机号码校验 */
+    Vertex *phoneRegexObj = CreateRegexObj("1(3|4|5|7|8)\\d\\d\\d\\d\\d\\d\\d\\d\\d");
+    bool result1 = RegexMatch("15973850842", phoneRegexObj);
+    bool result2 = RegexMatch("13333", phoneRegexObj);
     
     printf("%s \n", result1 ? "true" : "false");
-    
-    DestroyGrammerTable();
-    
-    const char *regex2 = "\\t";
-    Vertex *regexObj2 = CreateRegexObj(regex2);
-    bool result2 = RegexMatch("\t", regexObj2);
-    bool result3 = RegexMatch("66", regexObj2);
-    
     printf("%s \n", result2 ? "true" : "false");
+    
+    /* 普通校验 */
+    Vertex *regexObj = CreateRegexObj("a(b|c)*666t*");
+    bool result3 = RegexMatch("accbb666", regexObj);
+    bool result4 = RegexMatch("a666tttttt", regexObj);
+    
     printf("%s \n", result3 ? "true" : "false");
+    printf("%s \n", result4 ? "true" : "false");
     
     return 0;
 }
